65beaae364fca8c6a28e5bc0f96349b6ed3616ec
yjeongs22 <bisionary04@gmail.com>
2026-05-25 15:56:00 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,10 +19,6 @@ public class UserRaidInfo
     public string ServerName { get; set; } = "";
     public string ClassName { get; set; } = "";
     public string ItemLevel { get; set; } = "";
-    public bool IsSupport { get; set; }
-    public string RoleIcon { get; set; } = "";
-    //푸시제발돼라
-
     // 직업군 자동 분류 (서포터 판독기)
     public bool IsSupport => ClassName == "바드" || ClassName == "홀리나이트" || ClassName == "도화가";
     public string RoleIcon => IsSupport ? "🛡️서폿" : "⚔️딜러";
